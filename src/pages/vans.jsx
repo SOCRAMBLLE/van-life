096f5d41b1572f4bbc5f41d83e67b9448f6ad3ec
SOCRAMBLLE <mfragaprt@me.com>
2024-03-19 10:46:21 +0100

    Added functionality to filter options on vans page

--- src/pages/vans.jsx
+++ src/pages/vans.jsx
@@ -3,26 +3,56 @@ import VanCard from "../components/vanCard";
 
 export default function Vans() {
   const [vansData, setVansData] = useState(null);
+  const [vanFilter, setVanFilter] = useState("all");
 
   useEffect(() => {
     fetch("/api/vans")
       .then((res) => res.json())
       .then((data) => setVansData(data.vans));
   }, []);
+
+  const filteredVans = () => {
+    if (vanFilter === "all") {
+      return vansData;
+    } else {
+      return vansData.filter((van) => van.type === vanFilter);
+    }
+  };
+  console.log(vanFilter);
   return (
     <main className="vanspage--container">
       <h2>Explore our van options</h2>
       <div className="vanspage--filter">
         <div className="vanspage--filter-buttons">
-          <button>Simple</button>
-          <button>Luxury</button>
-          <button>Rugged</button>
+          <button
+            className={vanFilter === "simple" && "active"}
+            onClick={() => setVanFilter("simple")}
+          >
+            Simple
+          </button>
+          <button
+            className={vanFilter === "luxury" && "active"}
+            onClick={() => setVanFilter("luxury")}
+          >
+            Luxury
+          </button>
+          <button
+            className={vanFilter === "rugged" && "active"}
+            onClick={() => setVanFilter("rugged")}
+          >
+            Rugged
+          </button>
         </div>
-        <button className="vanspage--filter-clear">Clear filters</button>
+        <button
+          onClick={() => setVanFilter("all")}
+          className="vanspage--filter-clear"
+        >
+          Clear filters
+        </button>
       </div>
       {vansData ? (
         <section className="vanspage--vanscontainer">
-          {vansData.map((van) => (
+          {filteredVans().map((van) => (
             <VanCard
               id={van.id}
               key={van.id}
