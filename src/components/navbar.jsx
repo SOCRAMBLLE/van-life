05b40c06f411965b4966542ffe78cf00d4394ba7
SOCRAMBLLE <mfragaprt@me.com>
2024-06-07 13:50:21 +0200

    Add translation support to navbar component

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -3,9 +3,11 @@ import LoginIcon from "../assets/images/avatar-icon.png";
 import { useContext } from "react";
 import { AuthContext } from "../lib/auth";
 import LanguageSwitcher from "./languageSwitcher";
+import { useTranslation } from "react-i18next";
 
 export default function Navbar() {
   const { user } = useContext(AuthContext);
+  const { t } = useTranslation("navbar");
 
   return (
     <nav>
@@ -18,20 +20,20 @@ export default function Navbar() {
             to="/host"
             className={({ isActive }) => (isActive ? "active" : "")}
           >
-            Host
+            {t("host")}
           </NavLink>
         )}
         <NavLink
           to="/about"
           className={({ isActive }) => (isActive ? "active" : "")}
         >
-          About
+          {t("about")}
         </NavLink>
         <NavLink
           to="/vans"
           className={({ isActive }) => (isActive ? "active" : "")}
         >
-          Vans
+          {t("Vans")}
         </NavLink>
         <NavLink
           to="/login"
